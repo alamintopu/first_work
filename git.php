bc0fa15518918fc7f7cd864a743bb96512aab310
alamintopu <iknow.du@gmail.com>
2021-12-02 00:26:41 +0600

    ( Second Commit )

--- git.php
+++ git.php
@@ -1,5 +1,17 @@
-<?php
-$a = "Hello World!!!";
-ver_dump($a);
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Github</title>
+</head>
+<body>
 
-?>
+	<form action="" method="POST">
+		<div class="fieldname0">
+			<input type="text" name="firstname"><br>
+			<input type="password" name="password"><br>
+			<input type="button" value="Submit">
+		</div>
+	</form>
+
+</body>
+</html>
